e7feb0b95c5f1c7fecd2704db22a77f0a97762a3
Torquil MacLeod <engineer@torquilmacleod.com>
2020-05-10 09:53:30 +1200

    Add base serverless.yml for deploying CRA to torquilmacleod.com

---
+++ fe/build/precache-manifest.f3bbbe544908ca89544047ee56fc83a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d1a9eb43e472368d4f67fac7bd15d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61c3b3688a91e91baf3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6423892c43c5d7307f6a",
+    "url": "/static/js/2.0bfd74ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bfd74ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61c3b3688a91e91baf3",
+    "url": "/static/js/main.117a49db.chunk.js"
+  },
+  {
+    "revision": "8d544d70fd048f060473",
+    "url": "/static/js/runtime-main.2067d7b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
