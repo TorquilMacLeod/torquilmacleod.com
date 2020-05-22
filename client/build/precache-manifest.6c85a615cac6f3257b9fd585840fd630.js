4862f1643133713c497db0d08bf5dccb2eab90ae
Torquil MacLeod <engineer@torquilmacleod.com>
2020-05-22 20:16:59 +1200

    Full swap from Amplify to Serverless for front and back end deployment

---
+++ client/build/precache-manifest.6c85a615cac6f3257b9fd585840fd630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe645feedd52477759a47e6d689f7cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "476ca215c20230dbbd7c",
+    "url": "/static/css/2.8563b531.chunk.css"
+  },
+  {
+    "revision": "03111a814beed012c67b",
+    "url": "/static/css/main.aa0900b6.chunk.css"
+  },
+  {
+    "revision": "476ca215c20230dbbd7c",
+    "url": "/static/js/2.69b353b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69b353b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03111a814beed012c67b",
+    "url": "/static/js/main.92b58380.chunk.js"
+  },
+  {
+    "revision": "f46feb6f1dc1096faf39",
+    "url": "/static/js/runtime-main.c0f8b35c.js"
+  }
+]);
---
+++ client/src/components/blog/Blog.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Navbar } from '..';
+import PostList from './PostList';
+
+import './assets/blog.css';
+
+const initialPosts = {
+  "1": {
+    title: "All You Need To Know About Software",
+    date: "12/05/2020",
+    category: "Software",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis eros ante. Duis porta porttitor lorem. In hac habitasse platea dictumst. Proin accumsan, ligula blandit luctus vestibulum, dui diam consequat lacus, quis aliquet nisi orci sit amet ante. Mauris et velit elementum risus pellentesque aliquet eu eu sem. Pellentesque porta erat ut volutpat iaculis."
+  },
+  "2": {
+    title: "Why Is Everyone Talking About Web Design",
+    date: "12/05/2020",
+    category: "Game Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis eros ante. Duis porta porttitor lorem. In hac habitasse platea dictumst. Proin accumsan, ligula blandit luctus vestibulum, dui diam consequat lacus, quis aliquet nisi orci sit amet ante. Mauris et velit elementum risus pellentesque aliquet eu eu sem. Pellentesque porta erat ut volutpat iaculis."
+  },
+  "3": {
+    title: "The Hidden Agenda Of Cloud",
+    date: "12/05/2020",
+    category: "Web Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis eros ante. Duis porta porttitor lorem. In hac habitasse platea dictumst. Proin accumsan, ligula blandit luctus vestibulum, dui diam consequat lacus, quis aliquet nisi orci sit amet ante. Mauris et velit elementum risus pellentesque aliquet eu eu sem. Pellentesque porta erat ut volutpat iaculis."
+  }
+}
+
+const Blog = () => {
+
+  const [posts, setPosts] = useState(initialPosts);
+
+  useEffect(() => {
+    setPosts(initialPosts);
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="blogColumn">
+        <PostList
+          posts={posts}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Blog;
